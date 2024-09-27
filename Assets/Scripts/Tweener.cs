1a5bab6219bc95d5f53d086d05ff2d31a82a52d2
David <davidphanwork@gmail.com>
2024-09-27 22:37:32 +1000

    Initial commit: Added frame-rate independent movement of the fish

---
+++ Assets/Scripts/Tweener.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tweener : MonoBehaviour
+{
+    [SerializeField] private GameObject fish;
+    private Transform fishTransform;
+    private List<Tween> activeTweens;
+    private int currentTweenIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fishTransform = fish.GetComponent<Transform>();
+        activeTweens = new List<Tween>();
+        currentTweenIndex = 0;
+        activeTweens.Add(new Tween(fishTransform, new Vector3(1.0f, -1.0f, 0.0f), new Vector3(6.0f, -1.0f, 0.0f), Time.time, 2.0f));
+        activeTweens.Add(new Tween(fishTransform, new Vector3(6.0f, -1.0f, 0.0f), new Vector3(6.0f, -5.0f, 0.0f), Time.time, 2.0f));
+        activeTweens.Add(new Tween(fishTransform, new Vector3(6.0f, -5.0f, 0.0f), new Vector3(1.0f, -5.0f, 0.0f), Time.time, 2.0f));
+        activeTweens.Add(new Tween(fishTransform, new Vector3(1.0f, -5.0f, 0.0f), new Vector3(1.0f, -1.0f, 0.0f), Time.time, 2.0f));
+        //StartCoroutine(moveTween(activeTweens[currentTweenIndex]));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (currentTweenIndex < activeTweens.Count)
+        {
+            Tween activeTween = activeTweens[currentTweenIndex];
+            float timeFraction = (Time.time - activeTween.StartTime) / activeTween.Duration;
+
+            if (timeFraction < 1.0f)
+            {
+                // Tween is still in progress, perform the lerp
+                activeTween.Target.position = Vector3.Lerp(activeTween.StartPos, activeTween.EndPos, timeFraction);
+            }
+            else
+            {
+                // Tween is done, move to the next tween
+                currentTweenIndex++;
+                if (currentTweenIndex < activeTweens.Count)
+                {
+                    // Reset start time for the next tween
+                    activeTweens[currentTweenIndex].StartTime = Time.time;
+                }
+            }
+        }
+
+    }
+
+}
